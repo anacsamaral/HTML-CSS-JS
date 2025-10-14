7b909eb791b5f1f4dc2c749d92f89f48b22afee9
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-10-13 22:05:53 -0300

    DOM

--- Desenvolvimento Web/DOM - Aula1310/AulaJS-DOM-01-TEMPLATE/funcoes.js
+++ Desenvolvimento Web/DOM - Aula1310/AulaJS-DOM-01-TEMPLATE/funcoes.js
@@ -1,1 +1,31 @@
- 
+function pegaValor() {
+    /*
+        // Aqui só pega o texto digitado no input
+       alert(document.getElementById("nome").value);
+    
+       // Aqui pega o objeto inteiro
+       alert(document.getElementById("nome"));
+       alert(document.getElementById("teste")); // h1
+    
+       let objeto = document.getElementById("nome");
+       objeto.value = 'Texto Alterado!!!';
+    */
+}
+
+function teste() {
+    var elemento = document.getElementById("intro");
+    elemento.align = "center";
+    elemento.innerHTML = "<strong>teste modificado</strong>";
+
+    let eDiv = document.getElementById('main'); // objeto
+    let vetP = eDiv.getElementsByTagName('p'); // vetor de objetos
+    for (let i = 0; i < vetP.length; i++)
+        elemento.innerHTML += ' - ' + vetP[i].innerHTML;
+}
+
+function sugereNome()
+{ let pessoas = ["João", "José", "Maria", "Sebastião", "Ana"];
+  let doc=document.querySelector("#texto");
+  doc.textContent=
+         pessoas[(Math.floor(Math.random()*pessoas.length))]; 
+}
